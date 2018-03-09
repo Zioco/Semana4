b9b6c879f114c685717f0ce35a0c1b1db0921836
Zioco <zioco9725@gmail.com>
2018-03-09 18:19:19 -0500

    commit

---
+++ app/src/main/java/com/example/edutools_011/myapplication/Main3Activity.java
@@ -0,0 +1,40 @@
+package com.example.edutools_011.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m) {
+        getMenuInflater().inflate(R.menu.primer_menu,m);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.opcion1:
+                startActivity(new Intent(this,MainActivity.class));
+                return true;
+            case R.id.opcion2:
+                startActivity(new Intent(this,Main2Activity.class));
+                return true;
+            case R.id.opcion3:
+                startActivity(new Intent(this,Main3Activity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
